ebe0170c4624e16a9e649e774bec6b6c973dc90f
sputnik555 <79382246+sputnik555@users.noreply.github.com>
2021-12-08 14:38:34 +0300

    split ifmain code into functions

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def download_comics(url, filename):
 
     return response['alt']
 
+
 def get_current_comics_num():
 
     response = requests.get('https://xkcd.com/info.0.json')
@@ -24,44 +25,73 @@ def get_current_comics_num():
     return response.json()['num']
 
 
-if __name__ == '__main__':
+def download_random_comics_image(filename):
 
-    load_dotenv()
     num = random.randint(1, get_current_comics_num())
-    alt = download_comics('https://xkcd.com/{}/info.0.json'.format(num), "image.png")
+    return download_comics('https://xkcd.com/{}/info.0.json'.format(num), filename)
+
+
+def get_vk_upload_url(token, group_id):
 
     params = {
-        'access_token': os.getenv('TOKEN'),
+        'access_token': token,
         'v': '5.131',
-        'group_id': os.getenv('GROUP_ID'),
+        'group_id': group_id,
     }
+
     response = requests.get('https://api.vk.com/method/photos.getWallUploadServer', params)
     response.raise_for_status()
+    return response.json()['response']['upload_url']
 
-    upload_url = response.json()['response']['upload_url']
 
-    with open('image.png', 'rb') as file:
+def vk_upload_image(filename, upload_url, token, group_id):
+
+    with open(filename, 'rb') as file:
         files = {'photo': file}
         response = requests.post(upload_url, files=files)
         response.raise_for_status()
         response = response.json()
 
-    params['server'] = response['server']
-    params['hash'] = response['hash']
-    params['photo'] = response['photo']
+    params = {
+        'access_token': token,
+        'v': '5.131',
+        'group_id': group_id,
+        'server': response['server'],
+        'hash': response['hash'],
+        'photo': response['photo']
+    }
+
     response = requests.post('https://api.vk.com/method/photos.saveWallPhoto', params=params)
     response.raise_for_status()
+    response = response.json()
+
+    return response['response'][0]['owner_id'], response['response'][0]['id']
+
+
+def vk_wall_post(token, owner_id, id, alt):
 
     post_params = {
-        'access_token' : params['access_token'],
+        'access_token': token,
         'v': '5.131',
-        'owner_id' : -207984001,
-        'attachments' : 'photo{}_{}'.format(response['response'][0]['owner_id'],
-                                            response['response'][0]['id']),
-        'message' : alt
+        'owner_id': owner_id,
+        'attachments': 'photo{}_{}'.format(owner_id, id),
+        'message': alt
 
     }
+
     response = requests.post('https://api.vk.com/method/wall.post', params=post_params)
     response.raise_for_status()
+
+
+if __name__ == '__main__':
+
+    load_dotenv()
+    token = os.getenv('TOKEN')
+    group_id = os.getenv('GROUP_ID')
+
+    alt = download_random_comics_image('image.png')
+    upload_url = get_vk_upload_url(token, group_id)
+    owner_id, id = vk_upload_image('image.png', upload_url, token, group_id)
+    vk_wall_post(token, owner_id, id, alt)
 
     os.remove("image.png")
