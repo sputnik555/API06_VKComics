b194e0fdcc6888aa7ba6f4272a411dc8dfeec0c1
sputnik555 <79382246+sputnik555@users.noreply.github.com>
2022-05-07 02:35:36 +0300

    add exception for VK API errors

--- main.py
+++ main.py
@@ -3,12 +3,21 @@ from dotenv import load_dotenv
 import os
 import random
 
+class VKAPIError(Exception):
+    def __init__(self, error):
+        self.text = 'Error code:{}; Message:{}'.format(error['error_code'], error['error_msg'])
+
+
+def catch_vk_api_error(response):
+    if 'error' in response.keys():
+        raise VKAPIError(response['error'])
+
 
 def get_comics_image_url(num):
     response = requests.get('https://xkcd.com/{}/info.0.json'.format(num))
     response.raise_for_status()
-    response = response.json()
-    return response['img'], response['alt']
+    response_content = response.json()
+    return response_content['img'], response_content['alt']
 
 
 def download_file(url, filename):
@@ -40,11 +49,10 @@ def get_vk_upload_url(token, group_id):
 
     response = requests.get('https://api.vk.com/method/photos.getWallUploadServer', params)
     response.raise_for_status()
-    response = response.json()
-    if 'error' in response.keys():
-        raise requests.HTTPError(response['error'])
+    response_content = response.json()
+    catch_vk_api_error(response_content)
 
-    return response['response']['upload_url']
+    return response_content['response']['upload_url']
 
 
 def upload_vk_image(filename, upload_url):
@@ -54,8 +62,7 @@ def upload_vk_image(filename, upload_url):
 
     response.raise_for_status()
     response = response.json()
-    if 'error' in response.keys():
-        raise requests.HTTPError(response['error'])
+    catch_vk_api_error(response)
 
     return response['server'], response['hash'], response['photo']
 
@@ -72,11 +79,10 @@ def save_vk_wall_photo(token, group_id, server, hash, photo):
 
     response = requests.post('https://api.vk.com/method/photos.saveWallPhoto', params=params)
     response.raise_for_status()
-    response = response.json()
-    if 'error' in response.keys():
-        raise requests.HTTPError(response['error'])
+    response_content = response.json()
+    catch_vk_api_error(response_content)
 
-    return response['response'][0]['owner_id'], response['response'][0]['id']
+    return response_content['response'][0]['owner_id'], response_content['response'][0]['id']
 
 
 def post_vk_wall(token, group_id, owner_id, photo_id, alt):
@@ -91,6 +97,7 @@ def post_vk_wall(token, group_id, owner_id, photo_id, alt):
 
     response = requests.post('https://api.vk.com/method/wall.post', params=post_params)
     response.raise_for_status()
+    catch_vk_api_error(response.json())
 
 
 if __name__ == '__main__':
@@ -106,7 +113,7 @@ if __name__ == '__main__':
         server, file_hash, photo = upload_vk_image(filename, upload_url)
         owner_id, photo_id = save_vk_wall_photo(token, group_id, server, file_hash, photo)
         post_vk_wall(token, group_id, owner_id, photo_id, alt)
-    except requests.HTTPError as error:
-        print('Error code:{}; Message:{}'.format(error.args[0]['error_code'], error.args[0]['error_msg']))
+    except VKAPIError as error:
+        print(error.text)
     finally:
         os.remove(filename)
